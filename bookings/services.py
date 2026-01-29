9ba4b606ce8dbc9b956deaaabb79a93de6150804
Alihan serik <negrb@internet.ru>
2026-01-29 09:48:32 +0500

    Initial commit: Django booking service for Student A

--- bookings/services.py
+++ bookings/services.py
@@ -26,7 +26,7 @@ class AvailabilityService:
             Ответ от сервиса доступности
         """
         try:
-            self.base_url = settings.AVAILABILITY_SERVICE_URL + "/check-availability/"
+            self.base_url = settings.AVAILABILITY_SERVICE_URL
             url = f"{self.base_url}/check-availability/"
 
             # Формируем данные для отправки
